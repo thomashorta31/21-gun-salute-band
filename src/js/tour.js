28e71134c17659c1b7ba45c061de824ecb222774
thomashorta31 <tommyhorta@gmail.com>
2017-03-02 12:53:18 -0800

    pages

---
+++ src/js/tour.js
@@ -0,0 +1,46 @@
+const tour = [
+  {
+    city: 'Los Angeles',
+    dates: '3/29'
+    venue: 'Shrine Expo',
+    imgSrc: '/img/venue/shrine.gif',
+    lineup: 'Bring Me The Horizon, The American Nightmare, Underoath, Beartooth',
+  },
+  {
+    city: 'San Diego',
+    dates: '3/30'
+    venue: 'Soma Sports Arena',
+    imgSrc: '/img/venue/soma.jpg',
+    lineup: 'Bring Me The Horizon, The American Nightmare, Underoath, Beartooth',
+  },
+  {
+    city: 'Arizona',
+    dates: '4/1'
+    venue: 'Comerica Theatre',
+    imgSrc: '/img/venue/Comerica.gif',
+    lineup: 'Bring Me The Horizon, The American Nightmare, Underoath, Beartooth',
+  },
+  {
+    name: 'Jerry',
+    instrument: 'Bass',
+    imgSrc: '/img/jerry-bass.jpg',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
+
+const markup = `
+<div class="card-group">
+   ${tour.map(dates => `
+  <div class="card">
+    <img class="card-img-top img-fluid" src="${dates.imgSrc}" alt="${dates.city}">
+      <div class="card-block">
+        <h4 class="card-title">${dates.city}</h4>
+        <p class="card-text">${dates.lineup}</p>
+        <p class="card-text"><small class="text-muted">${dates.venue}</small></p>
+      </div>
+    </div>`).join('')}
+</div>
+`;
+
+const featured = document.querySelector('.tour');
+featured.innerHTML = markup;
